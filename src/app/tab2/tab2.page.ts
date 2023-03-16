496df6339ba7dd13ff87c38ad0b0cd940a005921
Gabriela Silva <vigabiesdivision@gmail.com>
2023-03-16 08:58:35 -0300

    programming the functions p1

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  //criando as variaveis
+  valor = 1;
+
+  botao1 = 1;
+  botao2 = 3;
+  botao3 = 6;
+  botao4 = 9;
+  botao5 = 12;
+
+  pontoT1 = 0;
+  pontoT2 = 0;
+
+  state = "default";
 
   constructor() {}
 
+  //adicionando os valores
+  botaoAdd1() {
+    this.valor = this.botao1;
+  }
+
+  botaoAdd2() {
+    this.valor = this.botao2;
+  }
+
+  botaoAdd3() {
+    this.valor = this.botao3;
+  }
+
+  botaoAdd4() {
+    this.valor = this.botao4;
+  }
+
+  botaoAdd5() {
+    this.valor = this.botao5;
+  }
 }
